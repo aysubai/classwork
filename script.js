ee6a800b4d68ccf91e1037ec936fb94cc168bdc2
aysubai <aysubayramova2745@gmail.com>
2024-05-18 09:59:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// keywords
+
+
+
+// var user = 'Aysu Bayramova'
+// console.log(user);
+
+
+
+
+// let firstName = 'Aysu'
+// let lastName = 'Bayramova'
+// let fatherName = 'Aliakber'
+// let age = '20'
+// let info = `name:${firstName} 
+// surname:${lastName}
+// fathername:${fatherName} 
+// age:${age}`
+// console.log(info);
+
+
+
+// let a = 1
+// let b = '1'
+// let c = 1
+// console.log(a + b + c);
+// console.log(b + a + c);
+// console.log(c + b + a);
+// console.log(c + a + b);
+// console.log(a * b * c);
+// console.log(b * a * c);
+// console.log(c * b * a);
+// console.log(c * a * b);
+
+
+
+// let firstName = 'Aysu'
+// let lastName = 'Bayramova'
+// let age = '20'
+
+// if (age == 20 || firstName !=='Aysu') {
+//     console.log('1');
+// }
+
+//  else if ('age === 20') {
+//     console.log('2');
+// }
+
+// else if ( age === 22) {
+//     console.log('3');
+// }
+
+// else { 
+//     console.log('4');
+// }
+
+
+
+
+// if (age == 21 || firstName !=='Aysu') {
+//     console.log('1');
+// }
+
+//  else if ('age === 20') {
+//     console.log('2');
+// }
+
+// else if ( age === 22) {
+//     console.log('3');
+// }
+
+// else { 
+//     console.log('4');
+// }
