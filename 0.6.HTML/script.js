042de21458862653249ca0dea51472799d03effd
aysubai <aysubayramova2745@gmail.com>
2024-06-02 11:33:20 +0400

    feat:form disabled

---
+++ 0.6.HTML/script.js
@@ -0,0 +1,56 @@
+// const usernameInput = document.querySelector('input[type="text"]');
+// const emailInput = document.querySelector('input[type="email"]');
+
+// usernameInput.addEventListener('input', () => {
+//   console.log("Username:", usernameInput.value);
+// });
+
+// emailInput.addEventListener('input', () => {
+//   console.log("Email:", emailInput.value);
+// });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.querySelector('form');
+  
+//     form.addEventListener('submit', (event) => {
+//       event.preventDefault(); 
+  
+//       const usernameInput = document.querySelector('input[type="text"]');
+//       const emailInput = document.querySelector('input[type="email"]');
+//       const passwordInput = document.querySelector('input[type="password"]');
+//       const fileInput = document.querySelector('input[type="file"]');
+//       const checkboxInput = document.querySelector('input[type="checkbox"]');
+//       const genderInputs = document.querySelectorAll('input[type="radio"]');
+  
+//       console.log("Username:", usernameInput.value);
+//       console.log("Email:", emailInput.value);
+//       console.log("Password:", passwordInput.value);
+//       console.log("File:", fileInput.value);
+//       console.log("Checkbox checked:", checkboxInput.checked);
+  
+//       genderInputs.forEach(input => {
+//         if (input.checked) {
+//           console.log("Gender:", input.value);
+//         }
+//       });
+//     });
+//   });
+  
+  
+    //   if (password !== confirmPassword) {
+    //     alert("Passwords do not match!");
+    //     return; 
+    //   }
+    //   console.log("Username:", username);
+    //   console.log("Email:", email);
+    //   console.log("Password:", password);
+    //   console.log("File:", file);
+    //   console.log("Checkbox checked:", isChecked);
+    //   console.log("Gender:", gender);
+    
+  
+
+
